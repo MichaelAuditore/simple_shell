72607cc0f038627fc3e1778e14b0daf194bf6bae
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-19 18:02:16 +0000

    [Add] the perfect shell loop

--- SSS/super-simple_shell.c
+++ SSS/super-simple_shell.c
@@ -10,40 +10,66 @@
  *
  * Return: Always 0.
  */
+void handle()
+{
+	write(STDOUT_FILENO, "\n#cisfun$ ", 10);
+	return;
+}
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char *argv[] = {"/bin/ls", "/home/vagrant/simple_shell/SSS", NULL};
 	int status;
-	size_t i = 0;
 	pid_t child;
 	char *buffer = NULL;
 	char *ptr;
 
 	while (1)
 	{
-		printf("#cisfun$ ");
-		getline(&buffer, &i, stdin);
-		ptr = strtok(buffer, "\n");
-		if (ptr == NULL)
-		child = fork();
-		if (child == -1)
+		int lenbuf = 0;
+		size_t len = 0;
+
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		signal(SIGINT, handle);
+		lenbuf = getline(&buffer, &len, stdin);
+
+		/* Sentencias a getline*/
+		if (lenbuf == 0)
+			continue;
+		if (lenbuf == -1)
 		{
-			perror("Error:");
-			free(ptr);
-		}
-		if (child == 0)
-		{
-			if (execve(ptr, argv, NULL) == -1)
-			{
-				perror("Error:");
-			}
-			free(ptr);
-		}
-		else
-		{
+			write(STDERR_FILENO, "\n./shell: Can't read the command or find EOF\n", 45);
 			free(buffer);
-			wait(&status);
+			exit(EXIT_FAILURE);
 		}
+		else if (lenbuf > 1)
+		{
+			child = fork();
+			ptr = strtok(buffer, "\n");
+			if (child == -1)
+				perror("Error:");
+
+			/*ejecuciones del proceso hijo*/
+			if (child == 0)
+			{
+				if (execve(ptr, argv, NULL) == -1)
+					perror("Error:");
+				free(ptr);
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				free(buffer);
+				wait(&status);
+			}
+		}
+		else if (lenbuf == 1)
+			free(buffer);
 	}
 	return (0);
 }
