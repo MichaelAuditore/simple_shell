8e2f887c6b012bed1ee3f6d5c9d1c0566db9e1bc
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-19 18:01:27 +0000

    [Add] Proves

--- Arguments/2-own_get_line.c
+++ Arguments/2-own_get_line.c
@@ -1,18 +1,11 @@
 #define _GNU_SOURCE
 #include "shell.h"
 
-void handle_ctrlC(int sig)
-{
-	write(1, "\n$ ", 3);
-}
-
 ssize_t _getline(char **buffer)
 {
-	static int len;
+	int len;
 
 	*buffer = (char *)malloc(1024);
-	signal(SIGINT, handle_ctrlC);
-	write(STDOUT_FILENO, "$ ", 2);
      while((len = read(STDIN_FILENO, *buffer, 1024)) == 0 && len != EOF);
 	return (len);
 }
