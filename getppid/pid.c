004fe9ed5a78a1c86ace964c494fcc2711ca2442
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-19 18:03:02 +0000

    [Add] proves

--- getppid/pid.c
+++ getppid/pid.c
@@ -8,11 +8,9 @@
  */
 int main(void)
 {
-	pid_t my_pid;
 	pid_t parent_pid;
 
-	my_pid = getpid();
 	parent_pid = getppid();
-	printf("%u\n%u\n", my_pid, parent_pid);
+	printf("%u\n", parent_pid);
 	return (0);
 }
