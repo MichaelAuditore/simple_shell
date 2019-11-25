8f25bd0586e9c804be33169d3d876db6049ecbba
MichaelAuditore <michael_v613@hotmail.com>
2019-11-25 17:33:04 +0000

    [Feature] Isatty Version

--- shell/simple_shell.c
+++ shell/simple_shell.c
@@ -1,5 +1,17 @@
 #include "shell.h"
 /**
+ *
+ *
+ *
+ */
+void write_error(char *name, char **buffer)
+{
+	write(1, name, _strlen(name));
+	write(1, ": 1: ", 5);
+	write(1, *buffer, _strlen(*buffer));
+	write(1, ": not found\n", 12);
+}
+/**
  * shell_loop - initialize the infinite loop for shell
  * to evaluate the input of user, and executes.
  * @argv: Pointer to arguments recieved
@@ -15,25 +27,22 @@ void shell_loop(char **argv)
 
 	while (1)
 	{
-		write(STDOUT_FILENO, "#cisfun$ ", 9);
-		signal(SIGINT, _signal);
-		len = getline(&buffer, &lenb, stdin);
+		if (isatty(0) == 1)
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+		signal(SIGINT, _signal), len = getline(&buffer, &lenb, stdin);
 		if (_strcmp(buffer, "exit\n") == 0 || (int)len == -1)
 		{
-			if ((int)len == -1)
+			if ((int)len == -1 && isatty(0) == 1)
 				write(1, "\n", 1);
 			break;
 		}
 		child = fork();
 		if (child == -1)
-		{
-			free(buffer);
-			perror("Error:");
-		}
+			free(buffer), perror("Error:");
 		if (child == 0)
 		{
 			if (execute(buffer) == -1)
-				perror("Error:");
+				write_error(argv[0], &buffer);
 			exit(EXIT_SUCCESS);
 		}
 		wait(&status);
